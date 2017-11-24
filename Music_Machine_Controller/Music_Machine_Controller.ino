79e85171250dfc41f74efbc79b6e3cb5ac7af059
otso <otso.sorvettula@aalto.fi>
2017-11-24 17:39:48 +0200

    Cleaning up for new version

---
+++ Music_Machine_Controller/Music_Machine_Controller.ino
@@ -0,0 +1,134 @@
+/**
+ * Mehackit Music Machine Solenoid Controller
+ * 
+ * Solenoid controller is a custom ATMEGA board that takes in MIDI
+ * and opens VIN current through a corresponding transistor i.e. triggers
+ * a salonoid based on the incoming MIDI.
+ * 
+ * A 3 bit DIP-switch defines the MIDI notes. 0 is notes 0-7, 1 is 8-15 etc.
+ * When a MIDI note on is received, a solenoid is triggered for 50ms. All midi
+ * is passed to MIDI through.
+ * 
+ * Otso Sorvettula / Mehackit 2017
+ */
+
+#include <MIDI.h>  // Add Midi Library
+#include <Servo.h>
+
+#define ledPin 13
+#define S1 2
+#define S2 3
+#define S3 4
+#define S4 5
+#define S5 6
+#define S6 7
+#define S7 8
+#define ammountOfSolenoids 7
+#define ammountOfNotes 9
+#define SERVO_1_PIN 9
+#define SERVO_2_PIN 10
+#define SERVO_1_NOTE 7
+#define SERVO_2_NOTE 8
+
+int solenoids[ammountOfSolenoids] = {S1, S2, S3, S4, S5, S6, S7};
+int dipPins[2] = {11,12};
+int scale = 0;
+long notesOff[ammountOfSolenoids];
+unsigned long noteLength = 30;
+unsigned long now = 0;
+int servo1Angle = 0;
+int servo2Angle = 0;
+Servo servo1;
+Servo servo2;
+
+//Create an instance of the library with default name, serial port and settings
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() {
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(S4, OUTPUT);
+  pinMode(S5, OUTPUT);
+  pinMode(S6, OUTPUT);
+  pinMode(S7, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  
+  for (int i = 0; i < 3; ++i) {
+    pinMode(dipPins[i], INPUT_PULLUP);
+  }
+
+  for (int i = 0; i < ammountOfSolenoids; i++) {
+    notesOff[i] = 0;
+  }
+
+  scale = getDipPinsValToInt();
+
+  MIDI.begin(MIDI_CHANNEL_OMNI); //Listen to all MIDI channels
+  MIDI.setHandleNoteOn(MyHandleNoteOn);
+  MIDI.setHandleNoteOff(MyHandleNoteOff);
+
+  servo1.attach(SERVO_1_PIN);
+  servo2.attach(SERVO_2_PIN);
+  servo1.write(servo1Angle);
+  servo2.write(servo2Angle);
+}
+
+void loop() { // Main loop
+  MIDI.read(); // Continuously check if Midi data has been received.
+  now = millis();
+  for (int i = 0; i < ammountOfSolenoids; i++) {
+    if (notesOff[i] < now) {
+      digitalWrite(solenoids[i], LOW);
+    }
+  }
+}
+
+int getDipPinsValToInt() {
+  byte result = 0;
+  for (int i = 0; i < 2; ++i) {
+    bool switchOn = digitalRead(dipPins[i]);
+    if (switchOn) {
+      result = result | (1 << i);
+    }
+  }
+  return (int) result;
+}
+
+// MyHandleNoteON is the function that will be called by the Midi Library
+// when a MIDI NOTE ON message is received.
+// It will be passed bytes for Channel, Pitch, and Velocity
+void MyHandleNoteOn(byte channel, byte pitch, byte velocity) {
+  digitalWrite(ledPin, HIGH);
+  int note = pitch - ammountOfNotes * scale;
+  if (note >= 0 && note < ammountOfNotes) {
+    if (note < ammountOfSolenoids) {
+      digitalWrite(solenoids[note], HIGH);
+      notesOff[note] = millis() + noteLength;  
+    } else {
+      turnServo(note);
+    }
+  }
+}
+
+
+
+void turnServo(int note) {
+  if (note == SERVO_1_NOTE) {
+    int a = (servo1Angle + 180) % 360; //this is allways 0 or 180
+    servo1.write(a);
+  } else if(note == SERVO_2_NOTE) {
+    int a = (servo2Angle + 180) % 360; //this is allways 0 or 180
+    servo1.write(a);
+  }
+
+}
+
+// MyHandleNoteOFF is the function that will be called by the Midi Library
+// when a MIDI NOTE OFF message is received.
+// * A NOTE ON message with Velocity = 0 will be treated as a NOTE OFF message *
+// It will be passed bytes for Channel, Pitch, and Velocity
+void MyHandleNoteOff(byte channel, byte pitch, byte velocity) {
+  digitalWrite(ledPin, LOW);
+}
+
---
+++ Music_Machine_Sequencer/Music_Machine_Sequencer.ino
@@ -0,0 +1,259 @@
+/**
+   Mehackit Music Machine Sequencer
+
+   Music Machine Sequencer is a Teensy based sequencer
+   with 8 knobs that choose between predefined sequences.
+
+
+
+   Otso Sorvettula / Mehackit 2017
+*/
+
+#include <AnalogSmooth.h>
+#include <MIDI.h>
+#include <Bounce.h>
+
+boolean useFirstNote[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+
+int midiNotes[8][2] = {
+  {0, 0},
+  {1, 1},
+  {2, 2},
+  {3, 3},
+  {4, 4},
+  {5, 5},
+  {6, 6},
+  {7, 7},
+};
+
+// Kick
+bool sequence1[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0},
+  {1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+  {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
+};
+
+// Snare
+const bool sequence2[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+};
+
+// Hihat
+const bool sequence3[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+  {0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1},
+  {0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0},
+  {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+  {1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+  {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1},
+};
+
+// Perc
+const bool sequence4[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+  {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1},
+  {0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+};
+
+
+const bool sequence5[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+  {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1},
+  {0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+};
+
+const bool sequence6[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0},
+  {1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+  {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
+};
+const bool sequence7[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0},
+  {1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+  {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
+};
+const bool sequence8[10][16] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0},
+  {1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+  {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
+};
+
+const int SEQ_LENGTH = 16;
+const bool (*sequences[])[16] {
+  sequence1,
+  sequence2,
+  sequence3,
+  sequence4,
+  sequence5,
+  sequence6,
+  sequence7,
+  sequence8,
+  NULL
+};
+
+const int tempoPin = A8;
+const int pin1 = A0;
+const int pin2 = A1;
+const int pin3 = A2;
+const int pin4 = A3;
+const int pin5 = A4;
+const int pin6 = A5;
+const int pin7 = A6;
+const int pin8 = A7;
+const int sequencePins[8] = {pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8};
+
+const int buttonPin = 2;
+Bounce pushbutton = Bounce(buttonPin, 10);
+byte buttonPressed = true;
+float swing = 0; //Swing in percentage
+float lastSwing = 0;
+float newSwing = 0;
+
+AnalogSmooth asTempo = AnalogSmooth(100);
+int tempo = 120;
+int newTempo = 120;
+
+long interval;
+unsigned long nextPosition = 0;
+int position = 0;
+int channel = 1;
+
+void setup() {
+  Serial.begin(9600);
+  MIDI.begin();
+  interval = bpmToInterval(tempo);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (micros() > nextPosition) {
+    if (position % 2 == 0) {
+      nextPosition = micros() + (interval * (1 - swing));
+    } else {
+      nextPosition = micros() + (interval * (1 + swing));
+    }
+    advance();
+  }
+
+  if (pushbutton.update()) {
+    if (pushbutton.fallingEdge()) {
+      lastSwing = asTempo.smooth(analogRead(tempoPin));
+      buttonPressed = true;
+      Serial.print("Button pressed: ");
+      Serial.println(lastSwing);
+
+      //Send random midi note
+      int i = random(8);
+      MIDI.sendNoteOn(midiNotes[i][0], 127, channel);
+      MIDI.sendNoteOn(midiNotes[i][1], 127, channel);
+    } else {
+      buttonPressed = false;
+      Serial.println("Button released");
+    }
+  }
+
+  if (buttonPressed) {
+    newSwing = asTempo.smooth(analogRead(tempoPin));
+    if (abs(newSwing - lastSwing) > 3) {
+      lastSwing = newSwing;
+      swing = (map(newSwing, 0, 1024, 0, 50)) / 100.0;
+      Serial.println(swing);
+    }
+  } else {
+    newTempo = asTempo.smooth(analogRead(tempoPin));
+    newTempo = map(newTempo, 0, 1024, 40, 240);
+    if (abs(newTempo - tempo) > 1) {
+      tempo = newTempo;
+      Serial.println(tempo);
+      interval = bpmToInterval(tempo);
+    }
+  }
+}
+
+unsigned long bpmToInterval(int bpm) {
+  unsigned long interval = 60 * 1000 * 1000 / bpm / 4;
+  return interval;
+}
+
+void advance() {
+  // Send midi off for all notes
+  for (int i = 0; i < 8; ++i) {
+    MIDI.sendNoteOff(midiNotes[i][0], 127, channel);
+    MIDI.sendNoteOff(midiNotes[i][1], 127, channel);
+  }
+
+  // Send midi on corresponding to the sequences
+  for (int i = 0; sequences[i]; i++) {
+    int seqNumber = map(analogRead(sequencePins[i]), 0, 1024, 0, 9);
+    Serial.println(seqNumber);
+    if (sequences[i][seqNumber][position]) {
+      if (useFirstNote[i]) {
+        //if(position % 2) {
+        MIDI.sendNoteOn(midiNotes[i][0], 127, channel);
+      } else {
+        MIDI.sendNoteOn(midiNotes[i][1], 127, channel);
+      }
+      useFirstNote[i] = !useFirstNote[i];
+    }
+  }
+
+  // Advance
+  position = (position + 1) % SEQ_LENGTH;
+}
+
+
